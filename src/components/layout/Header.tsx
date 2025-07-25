909574abedcf85ff747ef019a3df35acd9d8d46e
Kunj Patel <kunjp1157@gmail.com>
2025-07-25 06:06:41 +0000

    anyone click sports arena icon can redirect home page

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -33,7 +33,7 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 max-w-screen-2xl items-center">
-        <Link href="/facilities" className="mr-4 flex items-center space-x-2">
+        <Link href="/" className="mr-4 flex items-center space-x-2">
           <MountainSnow className="h-6 w-6 text-primary" />
           <span className="font-bold sm:inline-block text-lg font-headline">
             {siteName}
